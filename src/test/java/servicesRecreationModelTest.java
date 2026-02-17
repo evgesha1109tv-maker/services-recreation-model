3d6afd488c402de0f3061dd838ef24ac13c6e576
Evgen P <evgesha1109tv@gmail.com>
2026-02-17 22:02:18 +0400

    ParameterizedTest

--- src/test/java/servicesRecreationModelTest.java
+++ src/test/java/servicesRecreationModelTest.java
@@ -1,33 +1,22 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class servicesRecreationModelTest {
-    @Test
-    void Test1(){
+    @ParameterizedTest
+    // @CsvSource({"10000, 3000, 20000, 3","100000, 60000,150000,2"})
+    @CsvFileSource(files = "src/test/resources/income.expenses.threshold.csv")
+    void TestParameterized(int income, int expenses, int threshold, int expected) {
         servicesRecreationModel service = new servicesRecreationModel();
         // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
 
     }
-    @Test
-    void Test2(){
-        servicesRecreationModel service = new servicesRecreationModel();
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
 
-        Assertions.assertEquals(expected, actual);
-
-
-    }
 }
